2fd1525b326e394656305b72ec73283a0c43a456
ATE <akashsriyt@gmail.com>
2026-04-25 14:57:51 +0530

    real deploy

--- backend/server.js
+++ backend/server.js
@@ -12,6 +12,7 @@ const app = express();
 app.use(cors({
   origin: [
     process.env.FRONTEND_URL || 'http://localhost:5173',
+    'https://www.formfixer.in',
     'http://localhost:5173',
     'http://127.0.0.1:5173',
   ],
